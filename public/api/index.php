10023559c992857230708bd33945dbe4ec68a0b3
Tsukasa Takahashi <z19083z8@shibaurafzk.com>
2023-09-24 22:39:28 +0900

    Add api

---
+++ public/api/index.php
@@ -0,0 +1,16 @@
+<?php
+ini_set('display_errors', "On");
+
+header('Content-Type: application/json; charset=UTF-8');
+
+
+require "../functions.php";
+if (isset($_GET['k'])){
+    $db = new DBControlClass();
+
+    switch($_GET['k']) {
+    case "num_list":
+        $num_list = $db->get_exhibition_num_list();
+        $json_data = json_encode($num_list, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+        print $json_data;
+}
